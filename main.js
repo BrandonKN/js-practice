79ee1243230aa4ee25e96ea36160fb9afd05da83
BrandonKN <nicholsonb94@gmail.com>
2023-06-03 12:33:51 -0400

    js-practice-ex

---
+++ main.js
@@ -0,0 +1,35 @@
+let myfirstName = 'Brandon'
+let mylastName = 'Nicholson'
+let myAge = '29'
+
+// console.log(myfirstName)
+// console.log(mylastName)
+// console.log(myAge)
+
+// console.log('Hi, my name is ' + (myfirstName) + ' ' + (mylastName) + '.' + 'I am' + ' ' + (myAge) + ' ' + 'years old.')
+
+// console.log((myAge - 10))
+
+const person = {
+    'firstName' : 'Brandon',
+    'lastName' : 'Nicholson',
+    'age' : '29'
+}
+
+// console.log(person.firstName)
+// console.log(person.lastName)
+// console.log(person.age)
+
+// console.log('Hi, my name is ' + (person.firstName) + ' ' + (person.lastName) + '.' + 'I am' + ' ' + (person.age) + ' ' + 'years old.')
+
+// console.log((person.age - 10))
+
+const datsMe = ['Brandon', 'Nicholson', '29']
+
+// console.log(datsMe[0])
+// console.log(datsMe[1])
+// console.log(datsMe[2])
+
+// console.log('Hi, my name is ' + (datsMe[0]) + ' ' + (datsMe[1]) + '. ' + 'I am' + ' ' + (datsMe[2]) + ' ' + 'years old.')
+
+console.log((datsMe[2] - 10))
